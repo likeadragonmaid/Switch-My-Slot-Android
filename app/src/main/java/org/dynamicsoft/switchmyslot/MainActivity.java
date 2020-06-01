1f6bf483cb28664b19887fc90b1a4806131997f2
Shoukolate <shouko@airmail.cc>
2020-06-01 15:40:04 +0530

    Remove local bootctl library and fix bug that wasn't allowing to switch slot

--- app/src/main/java/org/dynamicsoft/switchmyslot/MainActivity.java
+++ app/src/main/java/org/dynamicsoft/switchmyslot/MainActivity.java
@@ -27,32 +27,25 @@
 package org.dynamicsoft.switchmyslot;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView halInfoTV, numberOfSlotsTV, currentSlotTV, CurrentSlotSuffixTV;
     int currentSlot;
     String convertedSlotNumberToAlphabet = null, path;
-    Switch switcher;
+    Button button;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -64,70 +57,51 @@ public class MainActivity extends AppCompatActivity {
         numberOfSlotsTV = findViewById(R.id.numberOfSlotsTV);
         currentSlotTV = findViewById(R.id.currentSlotTV);
         CurrentSlotSuffixTV = findViewById(R.id.CurrentSlotSuffixTV);
-        switcher = findViewById(R.id.switcher);
-
-        Context context = getApplicationContext();
-        path = "/data/data/org.dynamicsoft.switchmyslot/files/";
-        File file = new File(path);
-        if (!file.exists()) {
-            file.mkdir();
-            copyFile(context);
-        }
+        button = findViewById(R.id.button);
 
         try {
-            Runtime.getRuntime().exec("chmod +x /data/data/org.dynamicsoft.switchmyslot/files/bootctl");
+            Process phoneSupportedOrNotProcess;
+            phoneSupportedOrNotProcess = Runtime.getRuntime().exec("getprop | grep suffix");
+            DataInputStream phoneSupportedOrNotProcessOUT = new DataInputStream(phoneSupportedOrNotProcess.getInputStream());
+            if (phoneSupportedOrNotProcessOUT.equals("")){
+                Toast.makeText(this, "This is not an A/B device!", Toast.LENGTH_LONG).show();
+            }
+
             Process halInfoProcess;
-            halInfoProcess = Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl hal-info");
+            halInfoProcess = Runtime.getRuntime().exec("su -c bootctl hal-info");
             DataInputStream halinfoOUT = new DataInputStream(halInfoProcess.getInputStream());
             halInfoTV.setText(halinfoOUT.readLine());
 
             Process numberOfSlotsProcess;
-            numberOfSlotsProcess = Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl get-number-slots");
+            numberOfSlotsProcess = Runtime.getRuntime().exec("su -c bootctl get-number-slots");
             DataInputStream numberOfSlotsProcessOUT = new DataInputStream(numberOfSlotsProcess.getInputStream());
             numberOfSlotsTV.setText("Number of slots: " + numberOfSlotsProcessOUT.readLine());
 
             Process currentSlotProcess;
-            currentSlotProcess = Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl get-current-slot");
+            currentSlotProcess = Runtime.getRuntime().exec("su -c bootctl get-current-slot");
             DataInputStream currentSlotProcessOUT = new DataInputStream(currentSlotProcess.getInputStream());
 
             currentSlot = Integer.parseInt(currentSlotProcessOUT.readLine());
             if (currentSlot == 0) {
                 convertedSlotNumberToAlphabet = "A";
-                switcher.setChecked(false);
+                button.setText("Switch Slot to B");
             }
 
             if (currentSlot == 1) {
                 convertedSlotNumberToAlphabet = "B";
-                switcher.setChecked(true);
+                button.setText("Switch Slot to A");
             }
 
             currentSlotTV.setText("Current slot: " + convertedSlotNumberToAlphabet);
 
             Process CurrentSlotSuffixProcess;
-            CurrentSlotSuffixProcess = Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl get-suffix " + currentSlot);
+            CurrentSlotSuffixProcess = Runtime.getRuntime().exec("su -c bootctl get-suffix " + currentSlot);
             DataInputStream CurrentSlotSuffixProcessOUT = new DataInputStream(CurrentSlotSuffixProcess.getInputStream());
             CurrentSlotSuffixTV.setText("Current slot suffix: " + CurrentSlotSuffixProcessOUT.readLine());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        switcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                try {
-                    if (isChecked) {
-                        Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl set-active-boot-slot b");
-                        Toast.makeText(MainActivity.this, "Switching to Slot B", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Runtime.getRuntime().exec("su -c /data/data/org.dynamicsoft.switchmyslot/files/bootctl set-active-boot-slot a");
-                        Toast.makeText(MainActivity.this, "Switching to Slot A", Toast.LENGTH_SHORT).show();
-                    }
-                    Runtime.getRuntime().exec("su -c reboot");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     public void openRepo(View view) {
@@ -135,17 +109,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(browserIntent);
     }
 
-    private void copyFile(Context context) {
-        AssetManager assetManager = context.getAssets();
+    public void switchSlot(View view) {
         try {
-            InputStream in = assetManager.open("bootctl");
-            OutputStream out = new FileOutputStream(path + "bootctl");
-            byte[] buffer = new byte[1024];
-            int read = in.read(buffer);
-            while (read != -1) {
-                out.write(buffer, 0, read);
-                read = in.read(buffer);
+            if (currentSlot == 0) {
+                Runtime.getRuntime().exec("su -c bootctl set-active-boot-slot 1");
+            } if (currentSlot == 1) {
+                Runtime.getRuntime().exec("su -c bootctl set-active-boot-slot 0");
             }
+            Runtime.getRuntime().exec("su -c reboot");
         } catch (IOException e) {
             e.printStackTrace();
         }
