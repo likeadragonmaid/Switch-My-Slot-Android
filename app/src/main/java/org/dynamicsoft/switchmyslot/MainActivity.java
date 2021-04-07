1f07d4426c3b29fdd46940b8970af02e2842e94e
roihershberg <roihershberg@gmail.com>
2021-04-07 15:28:23 +0300

    Remove unnecessary imports
    Removed unnecessary imports.

--- app/src/main/java/org/dynamicsoft/switchmyslot/MainActivity.java
+++ app/src/main/java/org/dynamicsoft/switchmyslot/MainActivity.java
@@ -31,8 +31,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
